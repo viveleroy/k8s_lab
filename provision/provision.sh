ba26c05182eed49e43120ee224db722967de37c9
Roy Kliment <17612687+viveleroy@users.noreply.github.com>
2020-01-20 13:36:52 +0100

    switch cgroup driver to systemd

--- provision/provision.sh
+++ provision/provision.sh
@@ -8,7 +8,22 @@ sudo swapoff -a
 sudo apt-get update && sudo apt-get upgrade -y
 # Install Docker
 sudo apt-get install -y docker.io
+# Setup daemon.
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
 systemctl enable docker.service
+systemctl restart docker.service
 # Install kubeadm and kubectl
 # sudo sh -c "echo 'deb http://apt.kubernetes.io/ kubernetes-bionic main' >> /etc/apt/sources.list.d/kubernetes.list"
 sudo sh -c "curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -"
