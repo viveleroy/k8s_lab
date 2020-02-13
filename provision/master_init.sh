c6206b2a2046d768ef05514eb06c49e7dd80ffb8
Thijs Cramer <thijs.cramer@gmail.com>
2020-02-13 15:05:58 +0100

    Added sed to fix pod_cidr in calico

--- provision/master_init.sh
+++ provision/master_init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
+
 sudo kubeadm init --apiserver-advertise-address 192.168.33.20 --kubernetes-version 1.16.1 --pod-network-cidr 172.16.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
@@ -7,6 +8,10 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 # Download Calico plugin and RBAC YAML files and apply
 curl -sSL https://tinyurl.com/yb4xturm -o rbac-kdd.yaml
 curl -sSL https://tinyurl.com/y2vqsobb -o calico.yaml
+
+# Update POD CIDR for Calico
+sed -i 's/192.168.0.0\/16/172.16.0.0\/16/' calico.yaml 
+
 kubectl apply -f rbac-kdd.yaml
 kubectl apply -f calico.yaml
 kubectl get node
